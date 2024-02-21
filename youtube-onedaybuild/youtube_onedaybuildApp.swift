59ca23e50f9a20b34babed9bbb2b339e65c9a62e
meganWang <pollywd123@gmail.com>
2024-02-21 17:16:32 -0500

    initial project commit

---
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  youtube-onedaybuild
+//
+//  Created by Minghui Wang on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
